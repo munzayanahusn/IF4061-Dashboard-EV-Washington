4f4c8e6c064955ef9f078e5579730ac3555f63ee
Putinabillaa <aidiraputiabil@gmail.com>
2025-05-28 00:07:33 +0700

    chore: clean up unused files

--- src/components/CloroplethMap.js
+++
@@ -1,9 +0,0 @@
-import { useData } from "../hooks/useData";
-
-export default function CloroplethMap() {
-  const data = useData();
-
-  if (!data) return <p>Loading...</p>;
-
-  return <svg>{/* render your D3 visualization here */}</svg>;
-}
